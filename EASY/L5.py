7c1a71eb8c95f7284e90e82d4e71312dbd61da51
shubhankar71 <mshubhanakarrao@gmail.com>
2026-07-12 18:19:42 +0530

    editing

--- EASY/L5.py
+++ EASY/L5.py
@@ -2,9 +2,10 @@ ex = [40,10,20,30]
 # 10,20,30,40 == > 1,2,3,4
 def arrayRankTransform(arr):
     arr.sort()
+    #10,20,30,40 ==> 1,2,3,4
     num = []
     for i in range(len(arr)):
-        print(len(arr)-i)
+        print(i+1)
     
 
 print(arrayRankTransform(ex))
