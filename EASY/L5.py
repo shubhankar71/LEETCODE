03b1772982c52714ef3bf2494e018ca0748ad47c
shubhankar71 <mshubhanakarrao@gmail.com>
2026-07-13 21:04:11 +0530

    update solution

--- EASY/L5.py
+++ EASY/L5.py
@@ -1,11 +1,38 @@
-ex = [40,10,20,30]
-# 10,20,30,40 == > 1,2,3,4
-def arrayRankTransform(arr):
-    arr.sort()
-    #10,20,30,40 ==> 1,2,3,4
-    num = []
-    for i in range(len(arr)):
-        print(i+1)
-    
+'''
+You are given a 0-indexed integer array nums and a target element target. A target index is an index i such that nums[i] == target.
+Return a list of the target indices of nums after sorting nums in non-decreasing order.
+If there are no target indices, return an empty list. The returned list must be sorted in increasing order.
 
-print(arrayRankTransform(ex))
+ 
+
+Example 1:
+Input: nums = [1,2,5,2,3], target = 2
+Output: [1,2]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The indices where nums[i] == 2 are 1 and 2.
+
+Example 2:
+Input: nums = [1,2,5,2,3], target = 3
+Output: [3]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 3 is 3.
+
+Example 3:
+Input: nums = [1,2,5,2,3], target = 5
+Output: [4]
+Explanation: After sorting, nums is [1,2,2,3,5].
+The index where nums[i] == 5 is 4.
+'''
+
+ex = [1,2,5,2,3]
+t= 5
+def targetIndices(nums, target):
+    nums.sort()
+    num = []
+    for i in range(len(nums)):
+        if target == nums[i]:
+            num.append(i)
+    return num
+
+print(targetIndices(ex,t))
+    
