392882d0a10ddcc6e5481529097c63f4eff88dbb
MUSUDI SHUBHANKAR RAO <mshubhankarrao@gmail.com>
2026-07-14 23:02:01 +0530

    Update L7.py
    Signed-off-by: MUSUDI SHUBHANKAR RAO <mshubhankarrao@gmail.com>

--- EASY/L7.py
+++ EASY/L7.py
@@ -1,4 +1,6 @@
 '''
+Check if Numbers Are Ascending in a Sentence
+
 A sentence is a list of tokens separated by a single space with no leading or trailing spaces. Every token is either a positive number consisting of digits 0-9 with no leading zeros, 
 or a word consisting of lowercase English letters. For example, "a puppy has 2 eyes 4 legs" is a sentence with seven tokens: "2" and "4" are numbers and the other tokens such as "puppy" are words.
 Given a string s representing a sentence, you need to check if all the numbers in s are strictly increasing from left to right 
