0ea61a17da0d4b97f677222eecb65c2ca0f8456d
shubhankar71 <mshubhanakarrao@gmail.com>
2026-07-15 15:01:16 +0530

    Updated solution

--- MEDIUM/L4.py
+++ MEDIUM/L4.py
@@ -1,54 +1,17 @@
+w1 = "abcd"
+w2 = "pq"
 
-n = "1000"
-q = [
-    [0,3],
-    [1,1]
-]
-
-def sumAndMultiply(n, queries):
-    new_1= []
-    for start, end in queries:
-        word = (n[start:end+1].replace("0", ""))
-        if word == "":
-            new_1.append(0)
-            continue
-        new_1.append(word)
-        
-    print(new_1)
-    
-    new_2 = []
-    for i in range(len(new_1)):
-
-        if new_1[i] == 0:
-            new_2.append("")
-            continue
-
-        for j in range(len(new_1[i])):
-            new_2.append(int(new_1[i][j]))
-        new_2.append("")
-
-    print(new_2)
-    
-    multiplier = 0
+def mergeAlternately(word1, word2):
+    n = max(len(word1), len(word2))
     output = []
-    for i in range(len(new_2)):
-        if new_2[i] == "":
-            output.append(multiplier)
-            multiplier = 0
-            continue
-        multiplier += int(new_2[i])
-       
-    new_3 = []
+    for i in range(n):
+        if i>= len(word1):
+            output.append(word2[i])
+        elif i>= len(word2):
+            output.append(word1[i])
+        else:
+            output.append(word1[i])
+            output.append(word2[i])
+    return "".join(output)
 
-    for i in range(len(new_1)):
-
-        if new_1[i] == 0:
-            new_3.append(0)
-            continue
-
-        new_3.append(output[i] * int(new_1[i]))
-
-    print(new_3)
-
-            
-sumAndMultiply(n,q)
+print(mergeAlternately(w1,w2))
