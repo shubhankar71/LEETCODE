b43c63871b41e349c9ee1092bddee4b2e395cb8a
shubhankar71 <mshubhanakarrao@gmail.com>
2026-07-17 19:59:40 +0530

    Solution to my first Hard category question

--- HARD/L6.py
+++ HARD/L6.py
@@ -1,22 +1,56 @@
-import math
+'''
+Valid Number
+
+Given a string s, return whether s is a valid number.
+For example, all the following are valid numbers: "2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7",
+"+6e-1", "53.5e93", "-123.456e789", while the following are not valid numbers: "abc", "1a", "1e", "e3", "99e2.5", "--6", 
+"-+3", "95a54e53".
+
+Formally, a valid number is defined using one of the following definitions:
+1.An integer number followed by an optional exponent.
+2.A decimal number followed by an optional exponent.
+3.An integer number is defined with an optional sign '-' or '+' followed by digits.
+
+A decimal number is defined with an optional sign '-' or '+' followed by one of the following definitions:
+
+1.Digits followed by a dot '.'.
+2.Digits followed by a dot '.' followed by digits.
+3.A dot '.' followed by digits.
+4.An exponent is defined with an exponent notation 'e' or 'E' followed by an integer number.
+
+The digits are defined as one or more digits.
+
+
+Example 1:
+Input: s = "0"
+Output: true
+
+Example 2:
+Input: s = "e"
+Output: false
+
+Example 3:
+Input: s = "."
+Output: false
+'''
 def isNumber(s):
-    
-    
     try:
         int(s)
         return True
     except ValueError:
-        try:
-            float(s)
-            if math.isinf(float(s)) or math.isnan(float(s)):
+        try:  
+            if s.lower() in ("inf", "+inf", "-inf",
+                             "infinity", "+infinity", "-infinity",
+                             "nan", "+nan", "-nan"):
                 return False
+            float(s)
             return True
         except ValueError:
             return False
                 
         
 
-n = "e3"
+n = "-8115e957"
 
 
 print(isNumber(n))    
