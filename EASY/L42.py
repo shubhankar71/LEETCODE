a74bbbd2365bbea2e2a84605706522519f321a98
shubhankar71 <mshubhanakarrao@gmail.com>
2026-08-09 00:51:09 +0530

    solution to new problem

---
+++ EASY/L42.py
@@ -0,0 +1,13 @@
+''' 
+Power of Three
+
+
+'''
+
+class Solution:
+    def isPowerOfThree(self, n):
+        if n <= 0:
+            return False
+        while n %3 == 0:
+            n //= 3
+        return n == 1
