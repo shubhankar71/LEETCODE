ee8adf44d41a11ee1f92a50b2b4f5a0dc2a694cd
shubhankar71 <mshubhanakarrao@gmail.com>
2026-08-09 00:51:38 +0530

    merge remote changes

---
+++ EASY/L43.py
@@ -0,0 +1,13 @@
+''' 
+Power of Four
+
+
+'''
+
+class Solution:
+    def isPowerOfFour(self, n):
+        if n <= 0:
+            return False
+        while n %4 == 0:
+            n //= 4
+        return n == 1
